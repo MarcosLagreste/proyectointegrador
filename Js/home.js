d762106204395a9a48a69987b9855fe6e4a1f1d4
tobymontoya <montoyat@udesa.edu.ar>
2020-06-10 11:32:58 -0300

    Arreglos primer entrega

---
+++ Js/home.js
@@ -0,0 +1,7 @@
+//let proxy = "https://cors-anywhere.herokuapp.com/";
+//let url = proxy + "https://api.deezer.com/chart/0/tracks";
+
+//tracks https://api.deezer.com/track/
+
+//tracks.innerHTML += '<li>' + '<a href="track.html?id='+ track.id+ '">'+ track.title + '</a></li>'
+//"https://api.deezer.com/track/
