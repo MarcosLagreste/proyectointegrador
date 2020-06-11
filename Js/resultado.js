5e032e160514d3a968b5c2fcea41f05abeea8afc
Tomi <Tomi@MacBook-Air-de-Marcela-2.local>
2020-06-11 17:41:32 -0300

    intento y error

--- Js/resultado.js
+++ Js/resultado.js
@@ -1,13 +1,13 @@
 let queryString = location.search;
-console.log(queryString);
+//console.log(queryString);
 let searchParams = new URLSearchParams (queryString);
-console.log(searchParams);
+//console.log(searchParams);
 
 let search = searchParams.get("search");
 console.log(search)
 let proxy = "https://cors-anywhere.herokuapp.com/";
 let url = proxy + "https://api.deezer.com/search/artist?q=" + search;
-console.log(url);
+//console.log(url);
 
 fetch(url)
     .then (function(response){
@@ -27,9 +27,16 @@ fetch(url)
     })
 
 
-
-//let url1 = proxy +  "https://api.deezer.com/chart/0/tracks" + s
-
+let idTrack = searchParams.get('id');
+let url1 = proxy + "https://api.deezer.com/search/track?q=" + idTrack;
+let resultados1 = document.querySelector (".resultados1")
+fetch(url1)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (resultados1){
+        resultados1.innerHTML += '<li>' + resultados1.title + '</li>'
+    })
 //fetch(url1)
 //    .then (function(response){
 //        return response.json();
@@ -41,7 +48,7 @@ fetch(url)
 //        resultados1.for(function(resultado){
 //            resultados1.innerHTML += "<a href='detail2.html'> <li>" + resultado.title + "</li></a>"
 //        })
-//    .catch(function(error){
-//        console.log(error);
-//     })
+   .catch(function(error){
+    console.log(error);
+    })
     
