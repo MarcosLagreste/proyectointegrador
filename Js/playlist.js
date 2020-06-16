c4dee7e0b6cc209f09266d4e75c2b98769349b6c
Tomi <Tomi@MacBook-Air-de-Marcela-2.local>
2020-06-16 19:45:51 -0300

    reproductor playlist

--- Js/playlist.js
+++ Js/playlist.js
@@ -29,7 +29,8 @@ function buscarYMostrarTrack(idTrack){
             console.log(track);
             
             playlistWrapper.innerHTML +=  '<div>' + '<li>' + '<a href="detail2.html?type=track&id=' + track.id + '">' + track.title + '</a></li>' +'</div>' +  '<div>' + '<iframe src="' + track.preview + '" frameborder="0"></iframe>' + '</div>' 
-
+            let player = document.querySelector('iframe');
+            player.src = 'https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=500&height=200&color=007FEB&layout=dark&size=medium&type=tracks&id=' + idTrack + '&app_id=1'
         })
         .catch(function(errors){
             console.log(errors);
