1dd9183fd0a8a0688560eebe9761da9c673e2e24
tobymontoya <montoyat@udesa.edu.ar>
2020-06-16 18:49:30 -0300

    links

--- Js/home.js
+++ Js/home.js
@@ -18,26 +18,26 @@ fetch(url)
         console.log (datos)
 
         //carrousel
-        let albums1 = document.querySelector (".albums1")
-        albums1.innerHTML = "<img src='" +  resultados[0].artist.picture_xl + "'>"
+        let albums1 = document.querySelector (".albums1") 
+        albums1.innerHTML = "<a href='detail2.html?type=track&id="  +  resultados[0].id + "'>" + "<img src='" +  resultados[0].artist.picture_xl + "'>"
         let albums2 = document.querySelector (".albums2")
-        albums2.innerHTML = "<img src='" +  resultados[1].artist.picture_xl + "'>"
+        albums2.innerHTML = "<a href='detail2.html?type=track&id="  +  resultados[1].id + "'>" + "<img src='" +  resultados[1].artist.picture_xl + "'>"
         let albums3 = document.querySelector (".albums3")
-        albums3.innerHTML = "<img src='" +  resultados[2].artist.picture_xl + "'>"
+        albums3.innerHTML = "<a href='detail2.html?type=track&id="  +  resultados[2].id + "'>" + "<img src='" +  resultados[2].artist.picture_xl + "'>"
         let albums4 = document.querySelector (".albums4")
-        albums4.innerHTML = "<img src='" +  resultados[3].artist.picture_xl + "'>"
+        albums4.innerHTML = "<a href='detail2.html?type=track&id="  +  resultados[3].id + "'>" + "<img src='" +  resultados[3].artist.picture_xl + "'>"
         let albums5 = document.querySelector (".albums5")
-        albums5.innerHTML = "<img src='" +  resultados[4].artist.picture_xl + "'>"
+        albums5.innerHTML = "<a href='detail2.html?type=track&id="  +  resultados[4].id + "'>" + "<img src='" +  resultados[4].artist.picture_xl + "'>"
         let albums6 = document.querySelector (".albums6")
-        albums6.innerHTML = "<img src='" +  resultados[5].artist.picture_xl + "'>"
+        albums6.innerHTML = "<a href='detail2.html?type=track&id="  +  resultados[5].id + "'>" + "<img src='" +  resultados[5].artist.picture_xl + "'>"
         let albums7 = document.querySelector (".albums7")
-        albums7.innerHTML = "<img src='" +  resultados[6].artist.picture_xl + "'>"
+        albums7.innerHTML = "<a href='detail2.html?type=track&id="  +  resultados[6].id + "'>" + "<img src='" +  resultados[6].artist.picture_xl + "'>"
         let albums8 = document.querySelector (".albums8")
-        albums8.innerHTML = "<img src='" +  resultados[7].artist.picture_xl + "'>"
+        albums8.innerHTML = "<a href='detail2.html?type=track&id="  +  resultados[7].id + "'>" + "<img src='" +  resultados[7].artist.picture_xl + "'>"
         let albums9 = document.querySelector (".albums9")
-        albums9.innerHTML = "<img src='" +  resultados[8].artist.picture_xl + "'>"
+        albums9.innerHTML = "<a href='detail2.html?type=track&id="  +  resultados[8].id + "'>" + "<img src='" +  resultados[8].artist.picture_xl + "'>"
         let albums10 = document.querySelector (".albums10")
-        albums10.innerHTML = "<img src='" +  resultados[9].artist.picture_xl + "'>"
+        albums10.innerHTML = "<a href='detail2.html?type=track&id=" +  resultados[9].id + "'>" + "<img src='" +  resultados[9].artist.picture_xl + "'>"
         resultados.forEach (function(track){
             tracks.innerHTML += "<div class='estilocharts'><li>" + "<a href='detail2.html?type=track&id="  + track.id + "'>" + track.title + "</li>" + "<img src='" +  track.artist.picture + "'> </div>"
             
